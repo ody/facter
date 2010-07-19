ada00d8652cf5d8fc7e01395b5cfa9c6ec8389fe
Cody Herriges <c.a.herriges@gmail.com>
2010-07-19 01:11:38 -0700

    Just some code ordering.

--- lib/facter/ipaddress6.rb
+++ lib/facter/ipaddress6.rb
@@ -1,26 +1,5 @@
 Facter.add(:ipaddress6, :timeout => 2) do
   setcode do
-    require 'resolv'
-
-    begin
-      if hostname = Facter.value(:hostname)
-        ip = Resolv.getaddresses(hostname)
-        unless ip == "::1" or ip !=~ /(?>[0-9,a-f,A-F]+\:{0,2})+/
-          ip
-        end
-      else
-        nil
-      end
-      rescue Resolv::ResolvError
-        nil
-      rescue NoMethodError # i think this is a bug in resolv.rb?
-        nil
-    end
-  end
-end
-
-Facter.add(:ipaddress6, :timeout => 2) do
-  setcode do
   if hostname = Facter.value(:hostname)
     # we need Hostname to exist for this to work
     host = nil
@@ -38,6 +17,8 @@ Facter.add(:ipaddress6, :timeout => 2) do
   end
 end
 
+# OS dependant code that parses the output of
+# various networking tools.
 Facter.add(:ipaddress6) do
   confine :kernel => :linux
   setcode do
@@ -45,7 +26,7 @@ Facter.add(:ipaddress6) do
     output = %x{/sbin/ifconfig}
 
     output.scan(/inet6 addr: ((?>[0-9,a-f,A-F]*\:{0,2})+)/).each { |str|
-      unless str =~ /fe80\:/ or str =~ /\:\:1/
+      unless str !~ /fe80\:/ or str =~ /\:\:1/
         ip = str
       end
     }
@@ -62,7 +43,7 @@ Facter.add(:ipaddress6) do
     output = %x{/usr/sbin/ifconfig -a}
 
     output.scan(/inet6 ((?>[0-9,a-f,A-F]*\:{0,2})+)/).each { |str|
-      unless str =~ /fe80\:/ or str =~ /\:\:1/
+      unless str !~ /fe80\:/ or str =~ /\:\:1/
         ip = str
       end
     }
@@ -79,7 +60,7 @@ Facter.add(:ipaddress6) do
     output = %x{/sbin/ifconfig -a}
 
     output.scan(/inet6 ((?>[0-9,a-f,A-F]*\:{0,2})+)/).each { |str|
-      unless str =~ /fe80\:/ or str =~ /\:\:1/
+      unless str !~ /fe80\:/ or str =~ /\:\:1/
         ip = str
       end
       }
@@ -87,4 +68,32 @@ Facter.add(:ipaddress6) do
       ip
 
     end
+end
+
+# If command output parsing fails...
+# Uses ruby's own resolv class which queries
+# DNS and /etc/hosts.
+Facter.add(:ipaddress6, :timeout => 2) do
+  setcode do
+    require 'resolv'
+
+    begin
+      if hostname = Facter.value(:hostname)
+        Resolv.getaddresses(hostname).each { |str|
+          if str =~ /(?>[0-9,a-f,A-F]*\:{0,2})+/ and !~ /\:\:1/
+            ip = str
+        }
+        end
+
+      ip
+
+      else
+        nil
+      end
+      rescue Resolv::ResolvError
+        nil
+      rescue NoMethodError # i think this is a bug in resolv.rb?
+        nil
+    end
+  end
 end
